fb9ee209dc8ab81d1c786d0224678e27b0103774
Rainer Grimm <rainer@grimm-jaud.de>
2016-05-16 17:53:40 +0200

    Source for thread-local added.

--- source/threadLocal.cpp
+++ source/threadLocal.cpp
@@ -1,57 +1,37 @@
 // threadLocal.cpp
 
-#include <atomic>
-#include <chrono>
 #include <iostream>
-#include <random>
+#include <string>
+#include <mutex>
 #include <thread>
-#include <utility>
-#include <vector>
 
-constexpr long long size= 100000000;   
+std::mutex coutMutex;
 
-constexpr long long firBound=  25000000;
-constexpr long long secBound=  50000000;
-constexpr long long thiBound=  75000000;
-constexpr long long fouBound= 100000000;
+thread_local std::string s("hello from ");
 
-thread_local unsigned long long tmpSum= 0;
+void addThreadLocal(std::string const& s2){
 
-void sumUp(std::atomic<unsigned long long>& sum, const std::vector<int>& val, unsigned long long beg, unsigned long long end){
-    for (auto i= beg; i < end; ++i){
-        tmpSum += val[i];
-    }
-    sum.fetch_add(tmpSum,std::memory_order_relaxed);
+  s+=s2;
+  // protect std::cout
+  std::lock_guard<std::mutex> guard(coutMutex);
+  std::cout << s << std::endl;
+  std::cout << "&s: " << &s << std::endl;
+  std::cout << std::endl;
+
 }
 
 int main(){
 
   std::cout << std::endl;
 
-  std::vector<int> randValues;
-  randValues.reserve(size);
+  std::thread t1(addThreadLocal,"t1"); 
+  std::thread t2(addThreadLocal,"t2"); 
+  std::thread t3(addThreadLocal,"t3"); 
+  std::thread t4(addThreadLocal,"t4"); 
 
-  std::mt19937 engine;
-  std::uniform_int_distribution<> uniformDist(1,10);
-  for ( long long i=0 ; i< size ; ++i) randValues.push_back(uniformDist(engine));
- 
-  std::atomic<unsigned long long> sum{}; 
-  auto start = std::chrono::system_clock::now();
-  
-  std::thread t1(sumUp,std::ref(sum),std::ref(randValues),0,firBound);
-  std::thread t2(sumUp,std::ref(sum),std::ref(randValues),firBound,secBound);
-  std::thread t3(sumUp,std::ref(sum),std::ref(randValues),secBound,thiBound);
-  std::thread t4(sumUp,std::ref(sum),std::ref(randValues),thiBound,fouBound);   
-  
   t1.join();
   t2.join();
   t3.join();
   t4.join();
-  
-  std::chrono::duration<double> dur= std::chrono::system_clock::now() - start;
-  std::cout << "Time for addition " << dur.count() << " seconds" << std::endl;
-  std::cout << "Result: " << sum << std::endl;
-
-  std::cout << std::endl;
 
 }
