efee4974207c93c2d661f9708b76c330d3069568
Rainer Grimm <rainer@grimm-jaud.de>
2016-06-05 08:48:19 +0200

    Example to array added.

--- source/sizeof.cpp
+++ source/sizeof.cpp
@@ -1,14 +1,33 @@
 // sizeof.cpp
 
 #include <iostream>
-
+#include <array>
+#include <vector>
+ 
+ 
 int main(){
-    
-  std::cout << std::endl;
-
-  std::cout << "sizeof(char): " << sizeof(char) << std::endl;
-  std::cout << "sizeof(int): " << sizeof(int) << std::endl;
   
   std::cout << std::endl;
+  
+  std::cout << "sizeof(int)= " << sizeof(int) << std::endl;
+  
+  std::cout << std::endl;
+  
+  int cArr[10]= {1,2,3,4,5,6,7,8,9,10};
+  
+  std::array<int,10> cppArr={1,2,3,4,5,6,7,8,9,10};
+  
+  std::vector<int> cppVec={1,2,3,4,5,6,7,8,9,10};
+  
+  std::cout << "sizeof(cArr)= " << sizeof(cArr) << std::endl;  
+  
+  std::cout << "sizeof(cppArr)= " << sizeof(cppArr) << std::endl;
+  
+  std::cout << "sizeof(cppVec) = "   << sizeof(cppVec) + sizeof(int)*cppVec.capacity() << std::endl;
+  std::cout << "               = sizeof(cppVec): " << sizeof(cppVec) << std::endl;
+  std::cout << "               + sizeof(int)* cppVec.capacity(): "   << sizeof(int)* cppVec.capacity() << std::endl;
+
+  std::cout << std::endl;
+  
 
 }
