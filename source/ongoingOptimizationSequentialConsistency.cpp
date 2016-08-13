9622dd5c1f51311867b3cf2d0aa1496867175a94
Rainer Grimm <rainer@grimm-jaud.de>
2016-08-13 07:20:52 +0200

    New source file added.

---
+++ source/ongoingOptimizationSequentialConsistency.cpp
@@ -0,0 +1,25 @@
+// ongoingOptimizationSequentialConsistency.cpp
+
+#include <atomic>
+#include <iostream>
+#include <thread>
+
+std::atomic<int> x{0};
+std::atomic<int> y{0};
+
+void writing(){  
+  x.store(2000);  
+  y.store(11);
+}
+
+void reading(){  
+  std::cout << y.load() << " ";  
+  std::cout << x.load() << std::endl;
+}
+
+int main(){
+  std::thread thread1(writing);
+  std::thread thread2(reading);
+  thread1.join();
+  thread2.join();
+};
