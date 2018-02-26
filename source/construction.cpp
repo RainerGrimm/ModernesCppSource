cc3687b3b64ff47994228c669d4bf48f7b1c9df0
Rainer Grimm <rainer@grimm-jaud.de>
2018-02-26 07:44:56 +0100

    Added source files.

---
+++ source/construction.cpp
@@ -0,0 +1,48 @@
+// constructionWihtBraces.cpp
+
+#include <iostream>
+
+class MyVector{
+public:
+    MyVector(int, int){
+        std::cout << "MyVector(int, int)" << std::endl;
+    }
+    MyVector(std::initializer_list<int>){
+        std::cout << "MyVector(std::initalizer_list<int>)" << std::endl;
+    }
+};
+
+class MyVector1{
+public:
+    MyVector1(int, int){
+        std::cout << "MyVector1(int, int)" << std::endl;
+    }
+};
+
+class MyVector2{
+public:
+    MyVector2(int, int){
+        std::cout << "MyVector(std::initalizer_list<int>)" << std::endl;
+    }
+};
+
+int main(){
+    
+    std::cout << std::endl;
+    
+    MyVector(1, 2);
+    MyVector{1, 2};
+    
+    std::cout << std::endl;
+    
+    MyVector1{1, 2};
+    
+    std::cout << std::endl;
+    
+    MyVector2(1, 2);
+    
+    
+    
+    std::cout << std::endl;
+    
+}
---
+++ source/constructionWithBraces.cpp
@@ -0,0 +1,46 @@
+// constructionWihtBraces.cpp
+
+#include <iostream>
+
+class MyVector{
+public:
+    MyVector(int, int){
+        std::cout << "MyVector(int, int)" << std::endl;
+    }
+    MyVector(std::initializer_list<int>){
+        std::cout << "MyVector(std::initalizer_list<int>)" << std::endl;
+    }
+};
+
+class MyVector1{
+public:
+    MyVector1(int, int){
+        std::cout << "MyVector1(int, int)" << std::endl;
+    }
+};
+
+class MyVector2{
+public:
+    MyVector2(int, int){
+        std::cout << "MyVector(std::initalizer_list<int>)" << std::endl;
+    }
+};
+
+int main(){
+    
+    std::cout << std::endl;
+    
+    MyVector(1, 2);                       // (1)
+    MyVector{1, 2};                       // (2) 
+    
+    std::cout << std::endl;
+    
+    MyVector1{1, 2};                      // (3)
+    
+    std::cout << std::endl;
+    
+    MyVector2(1, 2);                      // (4)
+    
+    std::cout << std::endl;
+    
+}
