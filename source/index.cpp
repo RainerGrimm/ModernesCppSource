dab2671e935fe9a65e4ba6fa94f678204b1fb090
Rainer Grimm <rainer@grimm-jaud.de>
2018-03-26 07:43:36 +0200

    Added a new source file.

---
+++ source/index.cpp
@@ -0,0 +1,20 @@
+// index.cpp
+
+#include <iostream>
+#include <vector>
+
+int main(){
+    
+    std::cout << std::endl;
+    
+    std::vector<int> vec{};
+    
+    auto ind1 = vec.size() - 1 ;
+    int ind2 = vec.size() -1 ;
+    
+    std::cout << "ind1: " << ind1 << std::endl;
+    std::cout << "ind2: " << ind2 << std::endl;
+    
+    std::cout << std::endl;
+    
+}
