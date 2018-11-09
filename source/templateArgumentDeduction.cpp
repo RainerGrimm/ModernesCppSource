a97e93bc94a06ed2e183c3f3a51f7d2d264ddc1c
RainerGrimm <rainer@grimm-jaud.de>
2018-11-09 09:49:14 +0100

    Fixed a file.

--- source/templateArgumentDeduction.cpp
+++ source/templateArgumentDeduction.cpp
@@ -21,8 +21,8 @@ int main(){
   showMe(5.5);          // not showMe<double>(5.5);
   showMe(5);            // not showMe<int>(5);
     
-  ShowMe<double>(5.5);  // with C++17: ShowMe(5.5);
-  ShowMe<int>(5);       // with C++17: ShowMe(5);
+  ShowMe(5.5);          // not ShowMe<double>(5.5);
+  ShowMe(5);            // not ShowMe<int>(5);
   
   std::cout << std::endl;
     
