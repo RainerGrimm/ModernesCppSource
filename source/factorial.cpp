4171b49c3d964cbf06ccfd27bd2e3f9f7285ca97
RainerGrimm <rainer@grimm-jaud.de>
2019-01-04 08:28:20 +0100

    Added the new source file.

--- source/factorial.cpp
+++ source/factorial.cpp
@@ -2,23 +2,23 @@
 
 #include <iostream>
 
-template <int N>
+template <int N>                                                                 // (2)
 struct Factorial{
-  static int const value= N * Factorial<N-1>::value;
+    static int const value = N * Factorial<N-1>::value;
 };
 
-template <>
-  struct Factorial<1>{
-  static int const value = 1;
+template <>                                                                      // (3)
+struct Factorial<1>{
+    static int const value = 1;
 };
 
 int main(){
     
-  std::cout << std::endl;
-
-  std::cout << "Factorial<4>::value: " << Factorial<4>::value << std::endl;
-  std::cout << "Factorial<5>::value: " << Factorial<5>::value << std::endl;
-  
-  std::cout << std::endl;
+    std::cout << std::endl;
+    
+    std::cout << "Factorial<5>::value: " << Factorial<5>::value << std::endl;      // (1)
+    std::cout << "Factorial<10>::value: " << Factorial<10>::value << std::endl;
+    
+    std::cout << std::endl;
 
 }
