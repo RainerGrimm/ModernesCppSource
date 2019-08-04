7f2ba41a6094f61958aa72058c743f2c47e5df86
RainerGrimm <rainer@grimm-jaud.de>
2019-08-04 23:18:55 +0200

    Fixed a few typos.

--- source/scoped_thread.cpp
+++ source/scoped_thread.cpp
@@ -9,7 +9,7 @@ class scoped_thread{
   std::thread t;
 public:
   explicit scoped_thread(std::thread t_): t(std::move(t_)){
-    if ( !t.joinable()) throw std::logic_error("No thread");
+    if ( !t.joinable() ) throw std::logic_error("No thread");
   }
   ~scoped_thread(){
     t.join();
