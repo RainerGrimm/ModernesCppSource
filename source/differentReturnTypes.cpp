efc8909d11ab8655b458b1d808db144a61411129
RainerGrimm <rainer@grimm-jaud.de>
2019-08-12 08:24:33 +0200

    Added the new source file.

---
+++ source/differentReturnTypes.cpp
@@ -0,0 +1,16 @@
+// differentReturnTypes.cpp
+
+template <typename T>
+auto getValue(T x){
+  if (x < 0)          // int
+    return -1;
+  else if (x > 0)
+    return 1.0;       // double
+  else return 0.0f;   // float
+}
+
+int main(){
+
+    getValue(5.5);
+ 
+} 
