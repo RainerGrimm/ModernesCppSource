c24818e3bae705cf8bf0ce44ee11cbeaeb90fd59
RainerGrimm <rainer@grimm-jaud.de>
2019-09-09 07:21:37 +0200

    Added the new post.

---
+++ source/undefinedBehaviour.cpp
@@ -0,0 +1,26 @@
+// undefinedBehaviour.cpp
+
+#include <array>
+#include <iostream>
+
+int main(){
+
+    std::cout << std::endl;
+    
+    std::array<int, 1> myArr{};                      // (0)
+
+    int i{};                                         // (0)
+
+    myArr[i] = i++;                                  // (1) 
+    
+    std::cout << i << "  " << i++ << std::endl;      // (1)
+    
+    std::cout << std::endl;
+
+    int n = ++i + i;                                 // (1)
+
+    std::cout << "n: " << n << std::endl;  
+
+    std::cout << std::endl;
+
+}
