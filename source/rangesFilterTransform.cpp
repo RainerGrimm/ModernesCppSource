e0fe1ba092d1a9d21acce814e0a32dc881bcc670
Rainer Grimm <rainer@grimm-jaud.de>
2020-02-17 11:43:04 +0100

    Small changes in files:

--- source/rangesFilterTransform.cpp
+++ source/rangesFilterTransform.cpp
@@ -8,8 +8,8 @@ int main() {
 
     std::vector<int> numbers = {1, 2, 3, 4, 5, 6};
   
-    auto results = numbers | std::view::filter([](int n){ return n % 2 == 0;})
-                           | std::view::transform([](int n){ return n * 2;});
+    auto results = numbers | std::views::filter([](int n){ return n % 2 == 0;})
+                           | std::views::transform([](int n){ return n * 2;});
                            
     for (auto v: results) std::cout << v << " ";     // 4 8 12
 
--- source/rangesV3FilterTransform.cpp
+++ source/rangesV3FilterTransform.cpp
@@ -8,8 +8,8 @@ int main() {
 
     std::vector<int> numbers = {1, 2, 3, 4, 5, 6};
   
-    auto results = numbers | ranges::view::filter([](int n){ return n % 2 == 0; })
-                           | ranges::view::transform([](int n){ return n * 2; });
+    auto results = numbers | ranges::views::filter([](int n){ return n % 2 == 0; })
+                           | ranges::views::transform([](int n){ return n * 2; });
                            
     for (auto v: results) std::cout << v << " ";     // 4 8 12
 
