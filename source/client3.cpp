96e50de981532463043a69993cbcf9de070c76f8
RainerGrimm <rainer@grimm-jaud.de>
2020-05-25 08:11:50 +0200

    Added new source files.

---
+++ source/client3.cpp
@@ -0,0 +1,19 @@
+// client3.cpp
+
+import std.core;
+
+import math;
+
+int main() {
+    
+    std::cout << std::endl;   
+   
+    std::cout << "math::add(2000, 20): " << math::add(2000, 20) << std::endl;
+    
+    std::vector<int> myVec{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    
+    std::cout << "math::getProduct(myVec): " << math::getProduct(myVec) << std::endl;
+    
+    std::cout << std::endl;
+   
+}
---
+++ source/mathImplementationUnit.cpp
@@ -0,0 +1,13 @@
+// mathImplementationUnit.cpp
+
+module math;
+
+import std.core;
+
+int add(int fir, int sec){
+    return fir + sec;
+}
+
+int getProduct(const std::vector<int>& vec) {
+    return std::accumulate(vec.begin(), vec.end(), 1, std::multiplies<int>());
+}
