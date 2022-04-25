d2f2b5af067e1a95e116e096442bb3d5b57b9d6b
RainerGrimm <rainer@grimm-jaud.de>
2022-04-25 08:23:52 +0200

    Adde new source file.

--- source/conceptsDefinitionEqual.cpp
+++ source/conceptsDefinitionEqual.cpp
@@ -1,45 +1,38 @@
-// conceptsDefintionEqual.cpp
+// conceptsDefinitionEqual.cpp
 
+#include <concepts>
 #include <iostream>
 
-template<typename T>
-concept Equal =
-    requires(T a, T b) {
-        { a == b } -> bool;
-        { a != b } -> bool;
+template<typename T>                                     // (1)
+concept Equal = requires(T a, T b) {
+    { a == b } -> std::convertible_to<bool>;
+    { a != b } -> std::convertible_to<bool>;
 };
-
 
 bool areEqual(Equal auto a, Equal auto b){
   return a == b;
 }
 
-/*
-
-struct WithoutEqual{
+struct WithoutEqual{                                       // (2)
   bool operator==(const WithoutEqual& other) = delete;
 };
 
-struct WithoutUnequal{
+struct WithoutUnequal{                                     // (3)
   bool operator!=(const WithoutUnequal& other) = delete;
 };
 
-*/
-
 int main() {
-  
-  std::cout << std::boolalpha << std::endl;
-  
-  std::cout << "areEqual(1, 5): " << areEqual(1, 5) << std::endl;
-  
-  /*
-  
-  bool res = areEqual(WithoutEqual(),  WithoutEqual());
-  
-  bool res2 = areEqual(WithoutUnequal(),  WithoutUnequal());
-  
-  */
-  
-  std::cout << std::endl;
-  
+ 
+    std::cout << std::boolalpha << '\n';
+    std::cout << "areEqual(1, 5): " << areEqual(1, 5) << '\n';
+ 
+    /*
+ 
+    bool res = areEqual(WithoutEqual(),  WithoutEqual());    // (4)
+    bool res2 = areEqual(WithoutUnequal(),  WithoutUnequal());
+ 
+    */
+
+    std::cout << '\n';
+ 
 }
