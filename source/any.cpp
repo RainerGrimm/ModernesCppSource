477607fa207f1d958ae7c71ec36f20820a57fea6
Kerim FIRAT <kerimfirat@gmail.com>
2022-12-24 11:24:54 +0300

    Update any.cpp

--- source/any.cpp
+++ source/any.cpp
@@ -11,7 +11,7 @@ int main(){
     
     std::cout << std::boolalpha;
     
-    std::vector<std::any> anyVec(true,2017,std::string("test"),3.14,MyClass());
+    std::vector<std::any> anyVec{true,2017,std::string("test"),3.14,MyClass()};
     std::cout << "std::any_cast<bool>anyVec[0]: " << std::any_cast<bool>(anyVec[0]); // true
     int myInt= std::any_cast<int>(anyVec[1]);                                        
     std::cout << "myInt: " << myInt << std::endl;                                    // 2017
