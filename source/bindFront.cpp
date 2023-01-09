3f3851078ace27f66996f94706c6272169e7c851
RainerGrimm <rainer@grimm-jaud.de>
2023-01-09 08:03:43 +0100

    Fixed typos.

--- source/bindFront.cpp
+++ source/bindFront.cpp
@@ -13,27 +13,27 @@ auto plusLambda = [](int a, int b) {
 
 int main() {
     
-    std::cout << std::endl;
+    std::cout << '\n';
     
     auto twoThousandPlus1 = std::bind_front(plusFunction, 2000);
-    std::cout << "twoThousandPlus1(20): " << twoThousandPlus1(20) << std::endl;
+    std::cout << "twoThousandPlus1(20): " << twoThousandPlus1(20) << '\n';
     
     auto twoThousandPlus2 = std::bind_front(plusLambda, 2000);
-    std::cout << "twoThousandPlus2(20): " << twoThousandPlus2(20) << std::endl;
+    std::cout << "twoThousandPlus2(20): " << twoThousandPlus2(20) << '\n';
     
     auto twoThousandPlus3 = std::bind_front(std::plus<int>(), 2000);
-    std::cout << "twoThousandPlus3(20): " << twoThousandPlus3(20) << std::endl;
+    std::cout << "twoThousandPlus3(20): " << twoThousandPlus3(20) << '\n';
     
     std::cout << "\n\n";
     
     using namespace std::placeholders;
     
     auto twoThousandPlus4 = std::bind(plusFunction, 2000, _1);
-    std::cout << "twoThousandPlus4(20): " << twoThousandPlus4(20) << std::endl;
+    std::cout << "twoThousandPlus4(20): " << twoThousandPlus4(20) << '\n';
     
     auto twoThousandPlus5 =  [](int b) { return plusLambda(2000, b); };
-    std::cout << "twoThousandPlus5(20): " << twoThousandPlus5(20) << std::endl;
+    std::cout << "twoThousandPlus5(20): " << twoThousandPlus5(20) << '\n';
        
-    std::cout << std::endl;
+    std::cout << '\n';
     
 }
--- source/lambdaAndAuto.cpp
+++ source/lambdaAndAuto.cpp
@@ -11,27 +11,27 @@ using namespace std::placeholders;
 
 int main(){
 
-  std::cout << std::endl;
+  std::cout << '\n';
 
   // invoking the function object directly
-  std::cout << "1/2.0= " << [](int a, int b){ return divMe(a, b); }(1, 2.0) << std::endl;
+  std::cout << "1/2.0= " << [](int a, int b){ return divMe(a, b); }(1, 2.0) << '\n';
 
   // placeholders for both arguments
   auto myDivBindPlaceholder= [](int a, int b){ return divMe(a, b); };
-  std::cout << "1/2.0= " << myDivBindPlaceholder(1, 2.0) << std::endl;
+  std::cout << "1/2.0= " << myDivBindPlaceholder(1, 2.0) << '\n';
 
   // placeholders for both arguments, swap the arguments
   auto myDivBindPlaceholderSwap= [](int a, int b){ return divMe(b, a); };
-  std::cout << "1/2.0= " << myDivBindPlaceholderSwap(2.0, 1) << std::endl;
+  std::cout << "1/2.0= " << myDivBindPlaceholderSwap(2.0, 1) << '\n';
 
   // placeholder for the first argument
   auto myDivBind1St= [](int a){ return divMe(a, 2.0); };
-  std::cout<< "1/2.0= " << myDivBind1St(1) << std::endl;
+  std::cout<< "1/2.0= " << myDivBind1St(1) << '\n';
 
   // placeholder for the second argument
   auto myDivBind2Nd= [](int b){ return divMe(1, b); };
-  std::cout << "1/2.0= " << myDivBind2Nd(2.0) << std::endl;
+  std::cout << "1/2.0= " << myDivBind2Nd(2.0) << '\n';
 
-  std::cout << std::endl;
+  std::cout << '\n';
 
 }
